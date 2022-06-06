d97db88f6aef168765a146d70ac0376ed192911d
Abanob <60190283+abanobMorgan@users.noreply.github.com>
2022-06-06 15:53:43 +0200

    Time: 342 ms (5.05%), Space: 29.7 MB (37.81%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        '''
+          Since the lenght of both the ll could be different. So the pointers will meet at the end
+          and in such a situation will,
+          Swap the head pointers until the condition is not ment. 
+        '''
+        l1,l2 = headA,headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
