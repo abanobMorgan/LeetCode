85fd85a12179e31edeb7cf21d81936d2c75f1d26
Abanob <60190283+abanobMorgan@users.noreply.github.com>
2022-06-28 07:14:57 +0200

    Time: 499 ms (11.87%), Space: 14.8 MB (93.89%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -5,18 +5,22 @@ class Solution:
         for char in s:
             frequency[ord(char) - ord('a')] += 1
         frequency.sort(reverse=True)
-        
+
         delete_count = 0
         # Maximum frequency the current character can have
-        max_freq_allowed = len(s)
+        max_freq_allowed = int(frequency[0])
         # Iterate over the frequencies in descending order
-        for freq in frequency:
+        for freq in range(len(frequency)):
             # Delete characters to make the frequency equal the maximum frequency allowed
-            if freq > max_freq_allowed:
-                delete_count += freq - max_freq_allowed
-                freq = max_freq_allowed
+            if frequency[freq] > max_freq_allowed:
+                delete_count += frequency[freq] - max_freq_allowed
+                frequency[freq] = max_freq_allowed
 
+            
+            if frequency[freq] == 0 :
+                delete_count += sum(frequency[freq:])
+                return delete_count
             # Update the maximum allowed frequency
-            max_freq_allowed = max(0, freq - 1)
+            max_freq_allowed = max(0, frequency[freq] - 1)
             
         return delete_count
